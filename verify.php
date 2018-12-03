2369b9d6056451b718a213abbddf649bfabdb815
deborasilaban <deborasilaban.ds@gmail.com>
2018-12-04 01:23:01 +0700

    Commit Tubes Debora Silaban

---
+++ verify.php
@@ -0,0 +1,43 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+include 'config.php';
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$flag = 'true';
+//$query = $mysqli->query("SELECT email, password from users");
+
+$result = $mysqli->query('SELECT nama,email,password from user order by kode_login asc');
+
+if($result === FALSE){
+  die(mysql_error());
+}
+
+if($result){
+  while($obj = $result->fetch_object()){
+    if($obj->email === $username && $obj->password === $password) {
+
+      $_SESSION['username'] = $username;
+      $_SESSION['kode_login'] = $obj->kode_login;
+      $_SESSION['nama'] = $obj->nama;
+      header("location:index.php");
+    } else {
+
+        if($flag === 'true'){
+          redirect();
+          $flag = 'false';
+        }
+    }
+  }
+}
+
+function redirect() {
+  echo '<h1>Invalid Login! Redirecting...</h1>';
+  header("Refresh: 3; url=index.php");
+}
+
+
+?>
